31b8ea698fa400a30507ff28973e3fa69839458e
svhp <tomasz.geszka@gmail.com>
2023-05-29 19:55:27 +0200

    new models for importing and exporting data in Json format

---
+++ IS_Projekt/Models/JsonECommerceData.cs
@@ -0,0 +1,16 @@
+﻿namespace IS_Projekt.Models
+{
+  public class JsonECommerceData
+    {
+        public string DATAFLOW { get; set; }
+        public string LAST_UPDATE { get; set; }
+        public string freq { get; set; }
+        public string unit { get; set; }
+        public string ind_type { get; set; }
+        public string indic_is { get; set; }
+        public string geo { get; set; }
+        public int TIME_PERIOD { get; set; }
+        public double? OBS_VALUE { get; set; }
+        public string OBS_FLAG { get; set; }
+    }
+}
