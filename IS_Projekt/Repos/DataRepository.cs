88a68e2f3917ea2a6f491eb67db606841e0bef7b
tomas <tomasz.geszka@gmail.com>
2023-06-01 17:36:37 +0200

    Added DataRepo

---
+++ IS_Projekt/Repos/DataRepository.cs
@@ -0,0 +1,61 @@
+﻿using IS_Projekt.Database;
+using IS_Projekt.Extensions;
+using IS_Projekt.Models;
+
+namespace IS_Projekt.Repos {
+    public class DataRepository: IDataRepository {
+
+        private readonly ApplicationDbContext _context;
+
+        public DataRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<DataModel> GetDataById(int id) {
+            //return await _context.
+            throw new NotImplementedException();
+        }
+
+
+        public Task<DataModel> AddData(DataModel data) {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataModel> DeleteData(int id) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DataModel>> GetAllData() {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DataModel>> GetDataByCountry(string country) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DataModel>> GetDataByCountryAndYear(string country, int year) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DataModel>> GetDataByIndividualCriteria(string individualCriteria) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DataModel>> GetDataByUnitOfMeasure(string unitOfMeasure) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DataModel>> GetDataByYear(int year) {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<DataModel>> GetFilteredData(FilterCriteria filter) {
+            throw new NotImplementedException();
+        }
+
+        public Task<DataModel> UpdateData(DataModel data) {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IS_Projekt/Repos/IDataRepository.cs
@@ -0,0 +1,19 @@
+﻿using IS_Projekt.Extensions;
+using IS_Projekt.Models;
+
+namespace IS_Projekt.Repos {
+    public interface IDataRepository {
+        Task<DataModel> GetDataById(int id);
+        Task<IEnumerable<DataModel>> GetAllData();
+        Task<IEnumerable<DataModel>> GetDataByCountry(string country);
+        Task<IEnumerable<DataModel>> GetDataByYear(int year);
+        Task<IEnumerable<DataModel>> GetDataByIndividualCriteria(string individualCriteria);
+        Task<IEnumerable<DataModel>> GetDataByUnitOfMeasure(string unitOfMeasure);
+        Task<IEnumerable<DataModel>> GetDataByCountryAndYear(string country, int year);
+        Task<IEnumerable<DataModel>> GetFilteredData(FilterCriteria filter);
+        Task<DataModel> AddData(DataModel data);
+        Task<DataModel> UpdateData(DataModel data);
+        Task<DataModel> DeleteData(int id);
+        
+    }
+}
