99baa2ea9c03dc86e46d0bda0fe5caa2786732dc
svhp <tomasz.geszka@gmail.com>
2023-06-02 22:28:05 +0200

    changed register layout, added simple validation, added error messages

--- IS_Projekt/ClientApp/src/app/register/register.component.ts
+++ IS_Projekt/ClientApp/src/app/register/register.component.ts
@@ -9,28 +9,36 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent {
 
-  username = '';
-  password = '';
+    username = '';
+    email = '';
+    password = '';
+    message = '';
+    isError = false;
+    isSuccess = false;
 
   constructor(private authService: AuthService, private router: Router) { }
 
   register() {
     // Creating a user object
+    
     let user = {
       username: this.username,
+      email: this.email,
       password: this.password
     };
 
     this.authService.register(user).subscribe(
-      (response: any) => { // Success callback
-        // Store the token on successful registration
-        localStorage.setItem('access_token', response.token);
-        // Redirect to the login page
-        this.router.navigate(['/login']);
+      (response: any) => { 
+        this.message = 'Registration successful!';
+        this.isSuccess = true;
+        setTimeout(() => this.router.navigate(['/login']), 2000);
       },
       (error: any) => { // Error callback
-        // Handle the error
-        console.error(error);
+
+        this.message = error.error;
+        this.isError = true;
+
+        console.error(error.message);
       }
     );
   }
