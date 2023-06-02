ba4a1d18257a51a25a41ce8be48a0bdfb18bb132
svhp <tomasz.geszka@gmail.com>
2023-06-02 22:37:39 +0200

    added login, added new dtos, fixed userservice

--- IS_Projekt/Services/IUserService.cs
+++ IS_Projekt/Services/IUserService.cs
@@ -4,8 +4,9 @@ namespace IS_Projekt.Services
 {
     public interface IUserService
     {
-        Task<User?> CreateUser(string username, string password);
+        Task<User?> CreateUser(User userData);
         string GenerateToken(User user);
+        Task<User?> GetUserByUsername(string username);
         Task<IEnumerable<User>> GetUsers();
         bool VerifyPassword(User user, string providedPassword);
     }
